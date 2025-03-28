96c5828c0f59f7a299196cfaffcfe4f3e88f9312
John Scira <jscira@broadinstitute.org>
2025-03-28 17:03:25 -0400

    updating tests w/ fixes

--- ops_utils/tests/test_terra_utils.py
+++ ops_utils/tests/test_terra_utils.py
@@ -1,5 +1,5 @@
 import responses
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, mock_open, patch
 from ops_utils.requests_utils.request_util import RunRequest
 from ops_utils.terra_utils.terra_util import Terra, TerraGroups, TerraWorkspace
 from ops_utils.terra_utils.terra_workflow_configs import WorkflowConfigs
@@ -12,51 +12,43 @@ class TestTerraWorkspaceUtils:
 
     @responses.activate
     def test_get_workspace(self):
-        responses.patch("https://api.firecloud.org/api/workspaces/test_billing_project/test_workspace")
         responses._add_from_file(file_path="ops_utils/tests/data/get_workspace.yaml")
         workspace_info = self.workspace.get_workspace_info()
         assert workspace_info
 
     @responses.activate
     def test_get_workspace_bucket(self):
-        responses.patch("https://api.firecloud.org/api/workspaces/test_billing_project/test_workspace")
         responses._add_from_file(file_path="ops_utils/tests/data/get_workspace.yaml")
         workspace_bucket = self.workspace.get_workspace_bucket()
         assert workspace_bucket == "gs-bucket-id"
 
     @responses.activate
     def test_get_workspace_entity_info(self):
-        responses.patch("https://api.firecloud.org/api/workspaces/test_billing_project/test_workspace/entities")
         responses._add_from_file(file_path="ops_utils/tests/data/workspace_entity_info.yaml")
         entify_info = self.workspace.get_workspace_entity_info()
-        assert entify_info['idName'] == "file_metadata_id"
+        assert entify_info['file_metadata']['idName'] == "file_metadata_id"
 
 
     @responses.activate
     def test_get_workspace_acl(self):
-        responses.patch("https://api.firecloud.org/api/workspaces/test_billing_project/test_workspace/acl")
         responses._add_from_file(file_path="ops_utils/tests/data/workspace_acl_info.yaml")
         workspace_acl = self.workspace.get_workspace_acl()
         assert workspace_acl
 
     @responses.activate
     def test_get_workspace_metrics(self):
-        responses.patch("https://api.firecloud.org/api/workspaces/dsp-ops-gp-development-terra/js_test/entityQuery")
         responses._add_from_file(file_path="ops_utils/tests/data/workspace_metrics.yaml")
         workspace_metrics = self.workspace.get_gcp_workspace_metrics(entity_type='file_metadata')
         assert workspace_metrics
 
     @responses.activate
     def test_get_workspace_workflows(self):
-        responses.patch("https://api.firecloud.org/api/workspaces/test_billing_project/test_workspace/methodconfigs")
         responses._add_from_file(file_path="ops_utils/tests/data/get_workspace_workflows.yaml")
         workflows = self.workspace.get_workspace_workflows()
         assert workflows
 
     @responses.activate
     def test_check_workspace_public(self):
-        responses.patch("https://api.firecloud.org/api/workspaces/test_billing_project/test_workspace")
-        responses.patch("https://sam.dsde-prod.broadinstitute.org/api/resources/v2/workspace/test-workspace-guid/policies/reader/public")
         responses._add_from_file(file_path="ops_utils/tests/data/check_workspace_public.yaml")
         workspace_public = self.workspace.check_workspace_public()
         assert not workspace_public 
@@ -64,39 +56,35 @@ class TestTerraWorkspaceUtils:
 
     @responses.activate
     def test_create_workspace(self):
-        responses.patch("https://api.firecloud.org/api/workspaces")
         responses._add_from_file(file_path="ops_utils/tests/data/create_workspace.yaml")
-        new_workspace_metadata = self.workspace.check_workspace_public()
+        new_workspace_metadata = self.workspace.create_workspace()
         assert new_workspace_metadata
 
 
     @responses.activate
     def test_update_workspace_attributes(self):
-        responses.patch("https://api.firecloud.org/api/workspaces/test_billing_project/test_workspace/updateAttributes")
         responses._add_from_file(file_path="ops_utils/tests/data/update_workspace_attributes.yaml")
         attributes = [{"op": "AddUpdateAttribute","attributeName": "dataset_id","addUpdateAttribute": 'ex-dataset-guid'}]
-        updated_workspace_metadata = self.workspace.update_workspace_attributes(attributes=attributes)
-        assert updated_workspace_metadata
+        workspace_update = self.workspace.update_workspace_attributes(attributes=attributes)
+        assert workspace_update == None
 
 
     @responses.activate
     def test_update_user_acl(self):
-        responses.patch("https://api.firecloud.org/api/workspaces/test_billing_project/test_workspace/acl")
-        responses._add_from_file(file_path="ops_utils/tests/data/update_workspace_attributes.yaml")
-        update_acl = self.workspace.update_user_acl(email='test-account@integration-project.iam.gserviceaccount.com', access_level='reader')
-        assert update_acl
+        responses._add_from_file(file_path="ops_utils/tests/data/update_acl.yaml")
+        update_acl = self.workspace.update_user_acl(email='test-account@integration-project.iam.gserviceaccount.com', access_level='READER')
+        assert update_acl 
 
     @responses.activate
     def test_update_multiple_user_acl(self):
-        responses.patch("https://api.firecloud.org/api/workspaces/test_billing_project/test_workspace/acl")
         responses._add_from_file(file_path="ops_utils/tests/data/update_multiple_acl.yaml")
         acl_list = [ { "email": "test@broadinstitute.org", "accessLevel": "READER", "canShare": False, "canCompute": False, }, { "email": "test-account@integration-project.iam.gserviceaccount.com", "accessLevel": "READER", "canShare": False, "canCompute": False, } ]
         update_multiple_acl = self.workspace.update_multiple_users_acl(acl_list=acl_list, invite_users_not_found=False)
         assert update_multiple_acl
 
     @responses.activate
-    def test_put_library_metadata(self):
-        responses.patch("https://api.firecloud.org/api/workspaces/test_billing_project/test_workspace/acl")
+    def test_upload_metadata_metadata(self):
         responses._add_from_file(file_path="ops_utils/tests/data/put_library_metadata.yaml")
+        patch('__main__.open', mock_open(read_data="entity:sample_id\tsample_alias\nRP-123_ABC\tABC"))
         upload_metadata_res = self.workspace.upload_metadata_to_workspace_table("sample.tsv")
         assert upload_metadata_res
