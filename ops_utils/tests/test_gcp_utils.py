f6c7fab1ca85b14a6f54a63b7994de92ee98a317
John Scira <jscira@broadinstitute.org>
2025-04-24 16:22:34 -0400

    adding tests

--- ops_utils/tests/test_gcp_utils.py
+++ ops_utils/tests/test_gcp_utils.py
@@ -30,67 +30,56 @@ class TestGCPUtils:
 
     @responses.activate
     def test_copy_file(self):
-        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/")
-        self.gcp_client.copy_cloud_file()
-        pass
+        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/copy_file.yaml")
+        self.gcp_client.copy_cloud_file(src_cloud_path="gs://test_src_path/file001.bin", full_destination_path="gs://dest_bucket/file001.bin")
 
     @responses.activate
     def test_delete_file(self):
-        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/")
-        self.gcp_client.delete_cloud_file
-        pass
+        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/delete_file.yaml")
+        self.gcp_client.delete_cloud_file(full_cloud_path="gs://test_bucket/file002.bin")
     
     @responses.activate
     def test_move_file(self):
-        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/")
-        self.gcp_client.move_cloud_file
-        pass
+        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/move_file.yaml")
+        self.gcp_client.move_cloud_file(src_cloud_path='gs://test_bucket/file004.bin', full_destination_path='gs://test_bucket/file004_moved.bin')
 
     @responses.activate
     def test_get_filesize(self):
-        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/")
-        self.gcp_client.get_filesize
-        pass
+        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/get_filesize.yaml")
+        filesize = self.gcp_client.get_filesize(target_path="gs://test_bucket/file001.bin")
+        assert filesize == 21788
 
     @responses.activate
     def test_check_if_files_are_same(self):
-        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/")
-        self.gcp_client.validate_files_are_same()
-        pass
+        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/check_files_same.yaml")
+        files_identical = self.gcp_client.validate_files_are_same(src_cloud_path="gs://test_bucket/file001.bin", dest_cloud_path="gs://test_bucket/file002.bin")
+        assert not files_identical
 
-    def test_multithread_validation(self):
-        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/")
-        self.gcp_client.loop_and_log_validation_files_multithreaded()
-        pass
-
-
-    def test_move_copy_files(self):
-        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/")
-        self.gcp_client.move_or_copy_multiple_files()
-        pass
-
+    @responses.activate
     def test_read_file(self):
-        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/")
-        self.gcp_client.read_file()
-        pass
+        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/read_file.yaml")
+        data = self.gcp_client.read_file(cloud_path='gs://test_bucket/uploaded_test_file.txt')
+        assert data == "test\n\ndata\n\nhere"
 
-
+    @responses.activate
     def test_upload_blob(self):
         responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/")
         self.gcp_client.upload_blob()
         pass    
 
-
+    @responses.activate
     def test_get_md5(self):
         responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/")
         self.gcp_client.get_object_md5()
         pass    
 
+    @responses.activate
     def test_copy_to_cloud(self):
         responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/")
         self.gcp_client.copy_onprem_to_cloud()
         pass    
 
+    @responses.activate
     def test_set_acl_public(self):
         responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/")
         self.gcp_client.set_acl_public_read()
@@ -117,3 +106,13 @@ class TestGCPUtils:
         responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/")
         self.gcp_client.write_to_gcp_file
         pass        
+
+    def test_multithread_validation(self):
+        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/")
+        self.gcp_client.loop_and_log_validation_files_multithreaded()
+        pass
+
+    def test_move_copy_files(self):
+        responses._add_from_file(file_path="ops_utils/tests/data/gcp_util/")
+        self.gcp_client.move_or_copy_multiple_files()
+        pass    
