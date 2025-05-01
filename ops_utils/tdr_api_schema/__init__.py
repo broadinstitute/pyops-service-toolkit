c11001ef611b3d305247b89aba402fce72dd3e25
John Scira <jscira@broadinstitute.org>
2025-05-01 16:15:00 -0400

    docstring linting fixes

--- ops_utils/tdr_api_schema/__init__.py
+++ ops_utils/tdr_api_schema/__init__.py
@@ -1,9 +1,19 @@
+"""
+Constants and default values for interacting with TDR API.
+
+@private
+"""
 from enum import Enum
 from pydantic import BaseModel, Field
 from typing import Optional
 
 
 class DataTypeEnum(str, Enum):
+    """TDR data types.
+    
+    @private
+    """
+
     string = "string"
     boolean = "boolean"
     bytes = "bytes"
@@ -23,6 +33,11 @@ class DataTypeEnum(str, Enum):
 
 
 class Column(BaseModel):
+    """TDR Colum schema.
+    
+    @private
+    """
+
     name: str = Field(min_length=1, max_length=63)
     datatype: DataTypeEnum
     array_of: Optional[bool] = None
@@ -30,27 +45,52 @@ class Column(BaseModel):
 
 
 class RelationshipTerm(BaseModel):
+    """TDR Relathionship term schema.
+
+    @private
+    """
+
     table: str = Field(min_length=1, max_length=63)
     column: str = Field(min_length=1, max_length=63)
 
 
 class Relationship(BaseModel):
+    """TDR Relationship schema.
+    
+    @private
+    """
+
     name: str = Field(min_length=1)
     from_table: RelationshipTerm = Field(alias="from")
     to: RelationshipTerm
 
 
 class PartitionModeEnum(str, Enum):
+    """TDR partition modes.
+    
+    @private
+    """
+
     none = "none"
     date = "date"
     int = "int"
 
 
 class DatePartition(BaseModel):
+    """TDR date partition schema.
+    
+    @private
+    """
+
     column: str = Field(min_length=1, max_length=63)
 
 
 class IntPartition(BaseModel):
+    """TDR Int partition schema.
+    
+    @private    
+    """
+
     column: str = Field(min_length=1, max_length=63)
     min: int
     max: int
@@ -58,6 +98,11 @@ class IntPartition(BaseModel):
 
 
 class Table(BaseModel):
+    """TDR table schema.
+    
+    @private
+    """
+    
     name: str = Field(max_length=63, min_length=1)
     columns: list[Column]
     primaryKey: Optional[list[str]] = None
