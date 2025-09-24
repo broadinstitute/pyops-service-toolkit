5a3d822b65c6f27327e94b2a21bf17a3dd1eea49
Nareh Sahakian <sahakian@broadinstitute.org>
2025-09-24 09:26:58 -0400

    upgrade jira - get issues method

--- ops_utils/jira_util.py
+++ ops_utils/jira_util.py
@@ -1,7 +1,7 @@
 """Module for interacting with Jira tickets."""
 import os
 import logging
-from jira import JIRA
+from atlassian import Jira
 from google.cloud import secretmanager
 from typing import Optional
 
@@ -11,10 +11,10 @@ BROAD_INSTITUTE_SERVER = "https://broadinstitute.atlassian.net/"
 """The default Broad Jira server address"""
 
 
-class Jira:
+class JiraUtil:
     """
     A class to assist in interacting with JIRA tickets.
-    
+
     Provides functionality to help in updating tickets
     (adding comments, updating ticket fields, and transitioning statuses). Also provides a way to query
     existing JIRA tickets using certain filters. Assumes that an accessible JIRA API key is stored in
@@ -36,10 +36,10 @@ class Jira:
         """@private"""
         self.jira_api_key_secret_name = jira_api_key_secret_name
         """@private"""
-        self.jira = self._connect_to_jira()
+        self.jira_connection = self._connect_to_jira()
         """@private"""
 
-    def _connect_to_jira(self) -> JIRA:
+    def _connect_to_jira(self) -> Jira:
         """
         Obtain credentials and establish the Jira connection.
 
@@ -60,7 +60,7 @@ class Jira:
             name = f"projects/{self.gcp_project_id}/secrets/{self.jira_api_key_secret_name}/versions/latest"
             token = client.access_secret_version(name=name).payload.data.decode("UTF-8")
 
-        return JIRA(server=self.server, basic_auth=(jira_user, token))
+        return Jira(url=self.server, username=jira_user, password=token)
 
     def update_ticket_fields(self, issue_key: str, field_update_dict: dict) -> None:
         """
@@ -71,7 +71,7 @@ class Jira:
         - field_update_dict (dict): The field values to update. Formatted with the field ID as the key,
         and the updated value as the key's value.
         """
-        issue = self.jira.issue(issue_key)
+        issue = self.jira_connection.issue(issue_key)
         issue.update(fields=field_update_dict)
 
     def add_comment(self, issue_key: str, comment: str) -> None:
@@ -82,7 +82,7 @@ class Jira:
         - issue_key (str): The issue key to update
         - comment (str): The comment to add
         """
-        self.jira.add_comment(issue_key, comment)
+        self.jira_connection.add_comment(issue_key, comment)
 
     def transition_ticket(self, issue_key: str, transition_id: int) -> None:
         """
@@ -92,13 +92,13 @@ class Jira:
         - issue_key (str): The issue key to update
         - transition_id (int): The status ID to transition the issue to
         """
-        self.jira.transition_issue(issue_key, transition_id)
+        self.jira_connection.transition_issue(issue_key, transition_id)
 
     def get_issues_by_criteria(
             self,
             criteria: str,
             max_results: int = 200,
-            fields: Optional[list[str]] = None,
+            fields: Optional[str] = None,
             expand_info: Optional[str] = None
     ) -> list[dict]:
         """
@@ -108,13 +108,13 @@ class Jira:
         - criteria (str): The criteria to search for. This should be formatted in a supported JIRA search filter
         (i.e. `project = '{project}' AND sprint = '{sprint_name}' AND status = {status}`)
         - max_results (int): The maximum number of results to return. Defaults to 200.
-        - fields (list[string], optional): The fields to include in the return for each
-        ticket (i.e. `["summary", "status", "assignee"]`)
+        - fields (string, optional): The fields to include in the return for each
+        ticket separated by commas (i.e. `"summary,status,assignee"`)
 
         **Returns:**
         - list[dict]: The list of issues matching the criteria
         """
         logging.info(f"Getting issues by criteria: {criteria}")
         if fields:
-            return self.jira.search_issues(criteria, fields=fields, maxResults=max_results, expand=expand_info)
-        return self.jira.search_issues(criteria, maxResults=max_results, expand=expand_info)
+            return self.jira_connection.jql(criteria, fields=fields, limit=max_results, expand=expand_info)
+        return self.jira_connection.jql(criteria, limit=max_results, expand=expand_info)
